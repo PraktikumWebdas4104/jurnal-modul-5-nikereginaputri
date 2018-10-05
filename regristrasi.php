c501328506c3314c7e48d46f46211e3863b98723
nikereginaputri <43513339+nikereginaputri@users.noreply.github.com>
2018-10-05 10:23:20 +0700

    Update regristrasi.php
    NIKEREGINA_6701174080

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
-
-
-
-</body>
-</html>
+<form action="koneksi.php" method="POST">
+	Nim : 
+	<input type="number" name="nim" /><br /><br />
+	Nama : 
+	<input type="text" name="nama" /><br /><br />
+	Email :
+	<input type="text" placeholder="email" name="email" />@gmail.com<br /><br/>
+	<input type="submit" value="Daftar" />
+</form>
