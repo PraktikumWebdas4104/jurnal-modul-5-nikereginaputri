a4489507d1e319d97c3a92591e0d0fef33cef917
nikereginaputri <43513339+nikereginaputri@users.noreply.github.com>
2018-10-05 10:21:51 +0700

    Update koneksi.php
    NIKEREGINA_6701174080

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,30 @@
-<?php
+<?php 
 
-?>
+	if (isset($_POST['submit'])){
+		$koneksi = new mysqli("localhost","root"," ","pendaftaran");
+
+		if ($koneksi) {
+			$nim=$_POST['nim'];
+			$nama=$_POST['nama'];
+			$email=$_POST['email'];
+
+			$status=true;
+			 if (!is_int($nim) && strlen($nim)!=10) {
+			 	echo "nim 10 karakter";
+
+			 	$status=false;
+			 }
+			 if(strlen($nama)!=25){
+	echo "nama 25 karakter";
+
+			 }
+
+			$sql=$koneksi->query("
+				INSERT INTO 'pendaftaran'('nim','nama','email')VALUES(".'$nim'.",".'$nama'.",".'$email'.")");
+		echo "koneksi berhasil";
+
+	}else{
+		echo "koneksi  gagal";
+	}
+}
+ ?>
